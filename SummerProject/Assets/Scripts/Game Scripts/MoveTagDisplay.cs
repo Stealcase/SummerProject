f7b65867237826c3ca630e0f034052ff5699cc59
Benjamin Harbakk <stealcase@hotmail.com>
2018-06-28 20:16:53 +0200

    A lot of Icons

---
+++ SummerProject/Assets/Scripts/Game Scripts/MoveTagDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MoveTagDisplay : MonoBehaviour {
+    public TextMeshProUGUI textName;
+    public Image sprite;
+
+    public MoveTag move;
+
+	// Use this for initialization
+	void Start () {
+		if (move != null)
+        {
+            Prime(move);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Prime(MoveTag move)
+    {
+        this.move = move;
+        if (textName != null)
+        {
+            textName.text = move.displayName;
+        }
+        if (sprite != null)
+        {
+            sprite.sprite = move.sprite;
+        }
+
+    }
+}
